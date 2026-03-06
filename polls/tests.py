6513907afe63eb990187bfded206707a1025ded9
Gurleen Kaur <gurleenkaur@Mac.lan>
2026-03-06 11:50:56 -0500

    added unit tests for models and views

--- polls/tests.py
+++ polls/tests.py
@@ -1,1 +1,24 @@
-# Create your tests here.
+import datetime
+from django.test import TestCase
+from django.utils import timezone
+from .models import Question
+from django.urls import reverse
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        """
+        If no questions exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
